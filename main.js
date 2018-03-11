a9e011866867c2784da7467b05c5701fa8c4c375
Leo Lugo <leo.lugo84@gmail.com>
2018-03-11 14:37:07 -0700

    Project

---
+++ main.js
@@ -0,0 +1,22 @@
+
+var stuff = document.querySelector("#stuff")
+var elements = data.map(function(item) {
+	return `
+	<a href="${item.url}" target="_blank">
+							<div class="contentpic">
+								<img src="assets/tech.jpg">
+							</div>
+							<div class="contenttitle">${item.title}</div>
+							<div class="sellerandstars">
+								<div class="contentseller">${item.shop_name}</div>
+								<div class="contentstars">4stars</div>
+							</div>
+							<div class="contentprice">${item.price}</div>
+							<div class="contentdiscount">Only 3 left</div>
+						</a>
+	`
+})
+
+elements.forEach(function(el) {
+	stuff.innerHTML += el
+})
